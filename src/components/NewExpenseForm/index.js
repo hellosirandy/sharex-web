e6afbfdad3079078634164c785ab06d8f1dd173c
hellosirandy <sac840711@gmail.com>
2019-06-20 00:24:06 -0700

    working on new expense form

--- src/components/NewExpenseForm/index.js
+++ src/components/NewExpenseForm/index.js
@@ -1,40 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import { compose } from 'recompose';
 import styles from './styles';
 
 class NewExpenseForm extends React.PureComponent {
+  handleDateChange = (event) => {
+    console.log(event);
+  };
   render() {
     return (
       <div style={styles.container}>
-        <h3 style={styles.heading}>
+        <h4 style={styles.heading}>
           New Expense
-        </h3>
-        <InputGroup className="mb-3">
+        </h4>
+        <Form.Group className="mb-3">
           <FormControl
             placeholder="Title"
           // onChange={this.handleInputChange('email')}
           />
-        </InputGroup>
-        <InputGroup className="mb-3">
+        </Form.Group>
+        <Form.Group className="mb-3">
           <FormControl
-            placeholder="Amount"
+            placeholder="Total"
+            type="number"
           // onChange={this.handleInputChange('email')}
           />
-        </InputGroup>
-        <InputGroup className="mb-3">
+        </Form.Group>
+        <Form.Group className="mb-3">
           <FormControl
             placeholder="You paid"
+            type="number"
           // onChange={this.handleInputChange('email')}
           />
-        </InputGroup>
-        <InputGroup className="mb-3">
+        </Form.Group>
+        <Form.Group className="mb-3">
           <FormControl
             placeholder="You should pay"
+            type="number"
           // onChange={this.handleInputChange('email')}
           />
-        </InputGroup>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <FormControl
+            placeholder="Category"
+          // onChange={this.handleInputChange('email')}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <DatePicker
+            style={{ width: '100%' }}
+            customInput={
+              <FormControl
+                placeholder="Category"
+              />
+            }
+            onChange={this.handleDateChange}
+            placeholderText="Date"
+          />
+        </Form.Group>
         <Button variant="primary" block type="submit">
           Submit
         </Button>
