016be265ee8d0e94fe7b288415a609d6944d2022
hellosirandy <sac840711@gmail.com>
2019-06-19 23:09:26 -0700

    fetch data into table

--- src/pages/DebtsPage/index.js
+++ src/pages/DebtsPage/index.js
@@ -2,26 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
-import { Table } from '@material-ui/core';
+import { Table } from 'react-bootstrap';
+import moment from 'moment';
 import styles from './styles';
 import NavBar from '../../components/NavBar';
 import { getExpense } from '../../store/actions/expense';
 
 class DebtsPage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    props.onGetExpense();
+  }
   render() {
+    const { expenseIds, expenseTable } = this.props;
     return (
       <React.Fragment>
         <NavBar />
         <Table striped bordered hover>
           <thead>
             <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>Title</th>
+              <th>Time</th>
+              <th>Debt</th>
             </tr>
           </thead>
-          <tbody />
+          <tbody>
+            {expenseIds.map((id) => {
+              const expense = expenseTable[id];
+              return (
+                <tr key={id}>
+                  <td>{expense.title}</td>
+                  <td>{moment.utc(expense.date).local().format('MM/DD/YYYY')}</td>
+                  <td>{expense.paid - expense.shouldPay}</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </Table>
       </React.Fragment>
     );
@@ -31,6 +47,7 @@ class DebtsPage extends React.PureComponent {
 DebtsPage.propTypes = {
   expenseIds: PropTypes.array.isRequired,
   expenseTable: PropTypes.object.isRequired,
+  onGetExpense: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => {
