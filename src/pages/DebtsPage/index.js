7d298225695051b158bfbd46ac9719ce369651b3
hellosirandy <sac840711@gmail.com>
2019-06-19 22:37:39 -0700

    add table and integrate redux

--- src/pages/DebtsPage/index.js
+++ src/pages/DebtsPage/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { compose } from 'recompose';
+import { Table } from '@material-ui/core';
 import styles from './styles';
+import NavBar from '../../components/NavBar';
+import { getExpense } from '../../store/actions/expense';
 
 class DebtsPage extends React.PureComponent {
   render() {
     return (
-      <div />
+      <React.Fragment>
+        <NavBar />
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody />
+        </Table>
+      </React.Fragment>
     );
   }
 }
 
 DebtsPage.propTypes = {
+  expenseIds: PropTypes.array.isRequired,
+  expenseTable: PropTypes.object.isRequired,
 };
 
-export default compose()(DebtsPage);
+const mapStateToProps = (state) => {
+  return {
+    expenseIds: state.expense.expenseIds,
+    expenseTable: state.expense.expenseTable,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onGetExpense: () => dispatch(getExpense()),
+  };
+};
+
+export default compose(connect(mapStateToProps, mapDispatchToProps))(DebtsPage);
