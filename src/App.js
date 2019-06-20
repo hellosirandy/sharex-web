c85937d03f9833d7d95a71e2b72dfe49e64ad203
hellosirandy <sac840711@gmail.com>
2019-06-19 20:37:31 -0700

    integrate redux in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import AuthPage from './pages/AuthPage';
+import { checkAuthenticated } from './store/actions/auth';
 
-
-function App() {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/signin" component={AuthPage} />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/signin" component={AuthPage} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+App.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  onCheckAuthenticated: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: Boolean(state.auth.token),
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onCheckAuthenticated: () => dispatch(checkAuthenticated()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
