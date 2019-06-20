366f0a2793c89491f71f2a27ea3814e1dde7bb39
hellosirandy <sac840711@gmail.com>
2019-06-19 21:16:21 -0700

    integrate signout in dropdown

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Navbar, Nav, Dropdown } from 'react-bootstrap';
 import AccountBox from '@material-ui/icons/AccountBox';
 import { compose } from 'recompose';
 import styles from './styles';
+import { signOut } from '../../store/actions/auth';
 
 class NavBar extends React.PureComponent {
+  handleSignOutClick = () => {
+    this.props.onSignOut();
+  }
   render() {
     return (
       <Navbar bg="light" expand="lg">
@@ -15,23 +20,31 @@ class NavBar extends React.PureComponent {
           <Nav className="mr-auto">
             <Nav.Link href="/debts">Debts</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
-        <Dropdown navbar alignRight="false">
-          <Dropdown.Toggle variant="light" id="dropdown-basic">
-            {/* Dropdown Button */}
-            <AccountBox />
-          </Dropdown.Toggle>
+          <Dropdown navbar alignRight>
+            <Dropdown.Toggle variant="light" id="dropdown-basic">
+              {/* Dropdown Button */}
+              <AccountBox />
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Sign Out</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={this.handleSignOutClick}>Sign Out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Collapse>
+
       </Navbar>
     );
   }
 }
 
 NavBar.propTypes = {
+  onSignOut: PropTypes.func.isRequired,
 };
 
-export default compose()(NavBar);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSignOut: () => dispatch(signOut()),
+  };
+};
+
+export default compose(connect(null, mapDispatchToProps))(NavBar);
