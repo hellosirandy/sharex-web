96275f8e2ac2d6b8e6f3c780084741c73d7aa291
hellosirandy <sac840711@gmail.com>
2019-06-19 20:55:35 -0700

    create NavBar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'recompose';
+import styles from './styles';
+
+class NavBar extends React.PureComponent {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+NavBar.propTypes = {
+};
+
+export default compose()(NavBar);
