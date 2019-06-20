003e848b06b7d3d56462db5ea34cd49078280b64
hellosirandy <sac840711@gmail.com>
2019-06-19 20:44:52 -0700

    create HomePage

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'recompose';
+import styles from './styles';
+
+class HomePage extends React.PureComponent {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+HomePage.propTypes = {
+};
+
+export default compose()(HomePage);
