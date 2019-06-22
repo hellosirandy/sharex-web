03fb0f06a6c32e1bb3dc509f36a1e7829753af85
hellosirandy <sac840711@gmail.com>
2019-06-22 10:56:57 -0700

    create debts table

---
+++ src/components/DebtsTable/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { compose } from 'recompose';
+
+class DebtsTable extends React.PureComponent {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+DebtsTable.propTypes = {
+};
+
+export default compose()(DebtsTable);
